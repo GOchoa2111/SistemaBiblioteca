e767e88f880f7446a23d74e30c921e155f4e016a
GOchoa2111 <gochoa.2111@gmail.com>
2025-05-10 12:31:12 -0600

    Agregado archivo RegistroJava

---
+++ RegistroUsuario/src/main/java/error/registrousuario/RegistroUsuario.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package error.registrousuario;
+
+/**
+ *
+ * @author GOchoa
+ */
+public class RegistroUsuario {
+
+    public static void main(String[] args) {
+        System.out.println("Hola ingresa tu nombre de usuario");
+        
+        System.out.println("Hola ingresa aqui tu codigo");
+    }
+}
